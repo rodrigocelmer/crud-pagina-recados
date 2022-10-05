138ac0585c1c311fdc33d4bc6d5e20fc680ef76a
rodrigocelmer <rodrigocelmer@hotmail.com>
2022-10-04 22:01:48 -0300

    add util UserFileSystem.ts

---
+++ src/util/UserFileSystem.ts
@@ -0,0 +1,28 @@
+import { User } from "../model/User";
+import fs from "fs";
+
+ const dbPath = 'db.json';
+
+ export function getUserSync(): User[] {
+     if(!fs.existsSync(dbPath)){
+         return [];
+     }
+ 
+     const data = fs.readFileSync(dbPath);
+     const userJSON = JSON.parse(data.toString()) as any[];
+ 
+     return userJSON.map((user) =>
+         User.fill(
+             user.id,
+             user.name
+         )
+     );
+ }
+ 
+ export function saveUserSync(users: User[]): void {
+     const dataJSON = JSON.stringify(
+         users.map((user) => user.toJson())
+     )
+ 
+     fs.writeFileSync(dbPath, dataJSON);
+ }
