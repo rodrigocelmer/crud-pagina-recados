1d00952762db57ebad805a404c84670bb1b78a45
rodrigocelmer <rodrigocelmer@hotmail.com>
2022-10-13 21:27:31 -0300

    add cors + update port

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import appRoutes from './routes';
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 appRoutes(app);
 
-app.listen(8080, () => console.log('Servidor iniciado'));
+app.listen(process.env.PORT || 8080, () => console.log('Servidor iniciado'));
