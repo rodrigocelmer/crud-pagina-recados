8251b846d3dc1e545bd019089854ee75da01b935
rodrigocelmer <rodrigocelmer@hotmail.com>
2022-11-21 16:59:35 -0300

    set schema and entities path

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -4,15 +4,12 @@ import { DataSourceOptions } from "typeorm";
 const config: DataSourceOptions = {
     type: "postgres",
     url: process.env.DATABASE_URL,
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_DATABASE,
     synchronize: false,
     logging: false,
+    schema: "recados",
     ssl: {
         rejectUnauthorized: false,
     },
+    entities: ['src/database/entities/*.ts']
 };
 export default config;
