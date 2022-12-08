1771d6cfb9199d1222f6d841597064967e76e948
rodrigocelmer <rodrigocelmer@hotmail.com>
2022-12-08 19:38:13 -0300

    hot-fix deploy

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import { DataSourceOptions } from "typeorm";
 
+const rootDir = process.env.NODE_ENV === 'production' ? 'dist' : 'src';
+
 const config: DataSourceOptions = {
     type: "postgres",
     url: process.env.DATABASE_URL,
@@ -10,6 +12,6 @@ const config: DataSourceOptions = {
     ssl: {
         rejectUnauthorized: false,
     },
-    entities: ['src/database/entities/*.ts']
+    entities: [rootDir + '/database/entities/*']
 };
 export default config;
