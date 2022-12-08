b98da707f981957f77f399e9291de3cbbed87de1
rodrigocelmer <rodrigocelmer@hotmail.com>
2022-12-08 20:30:06 -0300

    config redis

---
+++ src/database/redis-helper.ts
@@ -0,0 +1,9 @@
+import Redis from 'ioredis';
+import 'dotenv/config';
+
+export const redisHelper = {
+    client: null as unknown as Redis,
+    connect() {
+        this.client = new Redis(process.env.REDIS_URL as string);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import appRoutes from './routes';
 import cors from "cors";
 import { pgHelper } from './database/pg-helper';
+import { redisHelper } from './database/redis-helper';
 
 const app = express();
 
@@ -11,6 +12,7 @@ appRoutes(app);
 
 pgHelper.connect()
 .then(() => {
+    redisHelper.connect();
     app.listen(process.env.PORT || 8080, () => console.log('Servidor iniciado'));
 })
 .catch((err) => console.log(err));
