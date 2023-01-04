eec16c479a9b46d9b38d0ba587dcf123c1207349
rodrigocelmer <rodrigocelmer@hotmail.com>
2023-01-04 20:54:14 -0300

    refact message.getById()

--- src/app/features/messages/repositories/message.repository.ts
+++ src/app/features/messages/repositories/message.repository.ts
@@ -25,13 +25,20 @@ export class MessageRepository{
         return messageEntity;
     }
 
-    async getById(msgId: string): Promise<MessageEntity>{
+    async getById(msgId: string): Promise<Message>{
         const manager = pgHelper.client.manager;
         const messageEntity = await manager.findOne(MessageEntity, {
             where: {id: msgId}
         }) as MessageEntity;
 
-        return messageEntity;
+        const message = Message.fill(
+            messageEntity.id,
+            messageEntity.description,
+            messageEntity.detail,
+            messageEntity.archieved
+        )
+
+        return message;
     }
 
     async remove(msgId: string): Promise<void>{
