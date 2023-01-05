b4168d39642c91eb9e62977b8a0231461eda3dcb
rodrigocelmer <rodrigocelmer@hotmail.com>
2023-01-04 21:42:07 -0300

    add test update message - skipped

---
+++ src/app/features/messages/usecases/update-message.usecase.ts
@@ -0,0 +1,33 @@
+import { Message } from "../../../models/message";
+import { MessageRepository } from "../repositories/message.repository";
+
+interface RequestData {
+    id: string;
+    description: string;
+    detail: string;
+}
+
+export class UpdateMessage {
+    private _messageRepository: MessageRepository;
+
+    constructor(messageRepository: MessageRepository){
+        this._messageRepository = messageRepository;
+    }
+
+    async execute(data: RequestData){
+        const message = await this._messageRepository.getById(data.id);
+
+        if(!message) {
+            throw new Error("Message not found");
+        }
+
+        await this._messageRepository.update(data.id, data.description, data.detail);
+
+        return Message.fill(
+            message.id,
+            message.description,
+            message.detail,
+            message.archieved
+        )
+    }
+}
---
+++ tests/app/features/messages/usecases/update-message.usecase.spec.ts
@@ -0,0 +1,49 @@
+import { UserRepository } from "../../../../../src/app/features/users/repositories/user.repository";
+import { MessageRepository } from "../../../../../src/app/features/messages/repositories/message.repository";
+import { CreateUser } from "../../../../../src/app/features/users/usecases/create-user.usecase";
+import { AddMessage } from "../../../../../src/app/features/messages/usecases/add-message.usecase";
+import { UpdateMessage } from "../../../../../src/app/features/messages/usecases/update-message.usecase";
+
+const makeRequestDataMsg = () => {
+    return {
+        description: "test description",
+        detail: "test detail"
+    }
+}
+
+const makeRequestDataUsr = () => {
+    return {
+        name: "John Doe", 
+        password: "john1234", 
+        email: "johndoe@johndoe.com"
+    }
+}
+
+describe("Test update message use case", () => {
+    test.skip("Test if message was update successfully", async () => {
+        const usrRepository = new UserRepository();
+        const msgRepository = new MessageRepository();
+        const usr = new CreateUser(usrRepository);
+        const msg = new AddMessage(msgRepository);
+
+        jest.spyOn(usrRepository, "create").mockResolvedValue();
+        jest.spyOn(msgRepository, "create").mockResolvedValue();
+
+        const user = await usr.execute(makeRequestDataUsr());
+        let message = await msg.execute(user.id, makeRequestDataMsg());
+
+        const sut = new UpdateMessage(msgRepository);
+        jest
+            .spyOn(msgRepository, "update")
+            .mockResolvedValue();
+
+        message = await sut.execute({
+            id: message.id,
+            description: "new test description",
+            detail: "new test detail"
+        })
+
+        expect(message.description).toBe("new test description");
+        expect(message.detail).toBe("new test detail");
+    })
+})
