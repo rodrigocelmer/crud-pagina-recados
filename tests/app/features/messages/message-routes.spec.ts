224ab3ce0829caa80f46c9894aed1789bb06ac4f
rodrigocelmer <rodrigocelmer@hotmail.com>
2023-01-04 22:06:58 -0300

    add route test - get all messages

--- tests/app/features/messages/message-routes.spec.ts
+++ tests/app/features/messages/message-routes.spec.ts
@@ -56,4 +56,35 @@ describe("Tests all messages routes. Message routes use Redis", () => {
         expect(response.status).toBe(200);
         expect(msgEntity).toBeTruthy();
     })
+
+    test("Tests if all messages are returned",async () => {
+        jest.setTimeout(10000);
+        
+        const user = await supertest(app)
+            .post("/users")
+            .send({
+                name: "John Doe", 
+                password: "john1234", 
+                email: "johndoe@johndoe.com"
+            });
+
+        userId = user.body.id;
+
+        const msgResp = await supertest(app)
+            .post(`/users/${userId}/messages`)
+            .send({
+                description: "test description",
+                detail: "test detail"
+            })
+
+        const response = await supertest(app)
+            .get(`/users/${userId}/messages`)
+
+        msgId = msgResp.body.id;
+
+        expect(response.status).toBe(200);
+
+        await supertest(app)
+            .delete(`/users/${userId}/messages/${msgId}`)
+    })
 })
